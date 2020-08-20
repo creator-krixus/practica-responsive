bf4fe9556e5ee5d30e8d79a5c3f307c21d4eafa7
Wilson <wilsonrueda2@gmail.com>
2020-08-20 02:07:28 -0500

    posicione los botones

---
+++ javascript/sl.js
@@ -0,0 +1,26 @@
+var imagenes = [
+    "/imagenes/wyg.svg",
+    "/imagenes/mg1.svg",
+    "/imagenes/mg2.svg",
+    "/imagenes/mg3.svg"
+];
+var num = 0;
+
+function siguiente(){
+    var slider =
+    document.getElementById("slider");
+    num++;
+    if( num >= imagenes.length){
+        num = 0;
+    }
+    slider.src = imagenes[num];
+};
+var num = 0;
+
+function anterior(){
+    var slider =
+    document.getElementById("slider")
+    num--;
+    if(num < 0) {num = imagenes.length-1};
+    slider.src = imagenes[num];
+};
